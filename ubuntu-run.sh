54024ea6906fcc4fb9a9106abc5886a8a4955378
Gabriel <adeilmazk@gmail.com>
2021-03-31 12:13:48 -0300

    Update ubuntu-run.sh

--- ubuntu-run.sh
+++ ubuntu-run.sh
@@ -1,5 +1,5 @@
 sudo apt update -y &>/dev/null
 sudo apt upgrade -y &>/dev/null
 sudo apt-get install -y build-essential kernel-package libncurses5-dev bzip2 libssl-dev ccache got aria2 clang brotli &>/dev/null
-git clone https://github.com/akhilnarang/scripts
-bash scripts/setup/android_build_env.sh
+git clone https://github.com/akhilnarang/scripts &>/dev/null
+bash scripts/setup/android_build_env.sh &>/dev/null
