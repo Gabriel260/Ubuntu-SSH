6c3d5a6f3cd0e4c1f741b2bef4e7fcce362b10de
Gabriel <adeilmazk@gmail.com>
2021-03-31 12:40:16 -0300

    Update ubuntu-run.sh

--- ubuntu-run.sh
+++ ubuntu-run.sh
@@ -3,5 +3,5 @@ sudo apt upgrade -y &>/dev/null
 sudo apt-get -qq install bc build-essential zip curl libstdc++6 git wget python gcc clang libssl-dev rsync flex bison ccache openjdk-8-jdk aria2 &>/dev/null
 git config --global user.name "Gabriel260"
 git config --global user.email "adeilmazk@gmail.com"
-git clone https://github.com/akhilnarang/scripts &>/dev/null
-bash scripts/setup/android_build_env.sh &>/dev/null
+#git clone https://github.com/akhilnarang/scripts &>/dev/null
+#bash scripts/setup/android_build_env.sh &>/dev/null
