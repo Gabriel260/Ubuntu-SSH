d146d208723de3df8ce3af46460764624f2ba675
Gabriel <adeilmazk@gmail.com>
2021-03-31 13:02:29 -0300

    Kak

--- ubuntu-run.sh
+++ ubuntu-run.sh
@@ -6,7 +6,7 @@ echo "downloading packages"
 sudo apt-get -qq install bc build-essential zip curl libstdc++6 git wget python gcc clang libssl-dev rsync flex bison ccache openjdk-8-jdk aria2 &>/dev/null
 git config --global user.name "Gabriel260"
 git config --global user.email "adeilmazk@gmail.com"
-git clone https://github.com/akhilnarang/scripts
+git clone https://github.com/akhilnarang/scripts
 echo "downloading more packages"
 bash scripts/setup/android_build_env.sh &>/dev/null
 git config --global color.ui true
